741f1ca83fe4d232ddb36e4938ff0cbb5f52bea2
Himel Sarder <143216886+Himel-Sarder@users.noreply.github.com>
2026-01-28 11:29:10 +0600

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-<img width="1326" height="729" alt="image" src="https://github.com/user-attachments/assets/86c1917f-290d-43c0-83ef-e19796e57073" />
+# go_moon
+
+A new Flutter project.
+
+## Getting Started
+
+This project is a starting point for a Flutter application.
+
+A few resources to get you started if this is your first Flutter project:
+
+- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
+- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
+
+For help getting started with Flutter development, view the
+[online documentation](https://docs.flutter.dev/), which offers tutorials,
+samples, guidance on mobile development, and a full API reference.
--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:lottie/lottie.dart';
+
 import './pages/home_page.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
   await Hive.initFlutter();
   await Hive.openBox('tasksBox');
-
   runApp(const App());
 }
 
@@ -23,7 +23,48 @@ class App extends StatelessWidget {
         scaffoldBackgroundColor: const Color.fromRGBO(31, 31, 31, 1.0),
         useMaterial3: true,
       ),
-      home: const HomePage(),
+      home: const SplashScreen(),
+    );
+  }
+}
+
+/// ---------------- SPLASH SCREEN ----------------
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  bool _navigated = false;
+
+  void _goHome(Duration duration) {
+    if (_navigated) return;
+    _navigated = true;
+
+    Future.delayed(duration, () {
+      if (!mounted) return;
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (_) => const HomePage()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Lottie.asset(
+          'assets/animation/loading.json',
+          width: 220,
+          repeat: false,
+          onLoaded: (composition) {
+            _goHome(composition.duration);
+          },
+        ),
+      ),
     );
   }
 }
